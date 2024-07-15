4eb48d6a1cd989bbf27c3e6352703ec35aa5a12a
Vallabhu PavanaGanga <140430476+pavanagangav@users.noreply.github.com>
2024-07-15 22:29:01 +0530

    Add files via upload

---
+++ javaClass/src/introduction/firstProgram.java
@@ -0,0 +1,8 @@
+package introduction;
+
+public class firstProgram {
+public static void main(String[] args) {
+	System.out.println("Hello world!");
+}
+	
+}
---
+++ javaClass/src/introduction/myself.java
@@ -0,0 +1,10 @@
+package introduction;
+
+public class myself {
+	public static void main(String[] args) {
+		System.out.println("my self pavani");
+		System.out.println("student of cybersecurity");
+		
+	}
+
+}
